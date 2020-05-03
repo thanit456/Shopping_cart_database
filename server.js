af5ca51232e781001fa7390c3a5632482edc26bf
thanit456 <too_w_o@hotmail.com>
2020-05-03 12:59:54 +0700

    created connection and defined CRUD but no test

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ MongoClient.connect(connectionString, {
     const db = client.db("shopping-cart");
     const itemsCollection = db.collection("items");
 
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
     // ? Maybe it doesn't have to use
@@ -45,6 +46,45 @@ MongoClient.connect(connectionString, {
         })
         .catch((error) => console.error(error));
     });
+
+    app.put("/items", (req, res) => {
+      itemsCollection
+        .findOneAndUpdate(
+          { name: req.body.name },
+          {
+            $set: {
+              amount: req.body.amount,
+              price_baht: req.body.price_baht,
+              description: req.body.description,
+              image_url: req.body.image_url,
+            },
+          },
+          {
+            upsert: true,
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          res.json("PUT change_price Success");
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app
+      .delete("/items", (req, res) => {
+        /*
+      itemsCollection.remove({
+        { name: req.body.name },
+        options
+      });
+      */
+        itemsCollection.deleteOne({ name: req.body.name });
+      })
+      .then((res) => {
+        console.log(res);
+        res.json(`Deleted an item`);
+      })
+      .catch((error) => collection.error(error));
 
     app.listen(3001, () => {
       console.log("listening on 3001");
